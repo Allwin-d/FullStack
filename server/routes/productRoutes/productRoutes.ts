29d8b0a51a6281d77d047354571a8eb932ba50d8
Allwin <allwinselva7@gmail.com>
2026-05-24 17:04:00 +0530

    Added Dynamic route for the product

--- server/routes/productRoutes/productRoutes.ts
+++ server/routes/productRoutes/productRoutes.ts
@@ -1,8 +1,11 @@
 import express, { Router } from "express";
-import getProducts from "../../controller/productController/productController";
+import { getSingleProduct } from "../../controller/productController/productController";
+import { getProducts } from "../../controller/productController/productController";
 
 const router = express.Router();
 
 router.get("/product", getProducts);
+
+router.get("/products/:id", getSingleProduct);
 
 export default router;
