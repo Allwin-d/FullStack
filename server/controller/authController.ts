dcb9e19712445947c62e04d3553f219da77f334d
Allwin <allwinselva7@gmail.com>
2026-06-04 23:52:07 +0530

    Implemented Login User Functionality for the Movie catalogue application

--- server/controller/authController.ts
+++ server/controller/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const registerUser = async (req: Request, res: Response) => {
   const { userName, email, password, role } = req.body;
@@ -36,4 +37,44 @@ const registerUser = async (req: Request, res: Response) => {
   });
 };
 
-export default registerUser;
+//im gonna loginwith the email and password
+const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const userData = await User.findOne({ email });
+
+  if (!userData) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid Credentials , Email doesn't exist",
+    });
+  }
+
+  const passwordExist = await bcrypt.compare(password, userData.password);
+  if (!passwordExist) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid Credentials , Incorrect password",
+    });
+  }
+
+  const accessToken = jwt.sign(
+    {//these data gets stored inside a token
+      userId: userData._id,
+      email: userData.email,
+      role: userData.role,
+    },
+    process.env.JWT_SECRET_KEY as string, //its a secret key
+    {
+      expiresIn: "30m", //token expires after 30 min 
+    },
+  );
+
+  res.status(200).json({
+    success: true,
+    message: "Logged In Successfully",
+    accessToken,
+  });
+};
+
+export { registerUser, loginUser };
