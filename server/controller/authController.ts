c22695191ec85f7957a296a628b2f0ff427dda6d
Allwin <allwinselva7@gmail.com>
2026-06-04 21:19:59 +0530

    Fixed bugs on registerUser controller

--- server/controller/authController.ts
+++ server/controller/authController.ts
@@ -5,8 +5,6 @@ import bcrypt from "bcrypt";
 const registerUser = async (req: Request, res: Response) => {
   const { userName, email, password, role } = req.body;
 
-  console.log("User Details : ", userName, email, password, role);
-
   const isUserExist = await User.findOne({
     $or: [{ userName }, { email }],
   });
@@ -17,29 +15,25 @@ const registerUser = async (req: Request, res: Response) => {
       message:
         "Either userName or email is not unique, try with a different userName or email",
     });
-  } else if (!isUserExist) {
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(password, salt);
-
-    await User.create({
-      userName,
-      email,
-      password: hashPassword,
-      role,
-    });
-
-    return res.status(201).json({
-      success: true,
-      userName: userName,
-      email: email,
-      role: role,
-    });
-  } else {
-    res.status(500).json({
-      success: false,
-      message: "Unknown issue",
-    });
   }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(password, salt);
+
+  await User.create({
+    userName,
+    email,
+    password: hashPassword,
+    role,
+  });
+
+  return res.status(201).json({
+    success: true,
+    message: "User Registered SuccessFully",
+    userName,
+    email,
+    role,
+  });
 };
 
 export default registerUser;
